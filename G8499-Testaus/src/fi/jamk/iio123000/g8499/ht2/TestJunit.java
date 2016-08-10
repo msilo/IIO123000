6fdbc3a9b35616636fb9515dc241280840b81d94
G8499 <g8499@student.jamk.fi>
2016-08-10 17:05:35 +0700

    Small changes

--- G8499-Testaus/src/fi/jamk/iio123000/g8499/ht2/TestJunit.java
+++ G8499-Testaus/src/fi/jamk/iio123000/g8499/ht2/TestJunit.java
@@ -8,7 +8,7 @@ public class TestJunit {
    String message = "Hello World";	
    MessageUtil messageUtil = new MessageUtil(message);
 
-   @Test
+   
    public void testPrintMessage() {	  
       assertEquals(message,messageUtil.printMessage());
    }
