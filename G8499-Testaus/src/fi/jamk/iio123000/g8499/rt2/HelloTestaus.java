d6b5f2fbfc6e5c1d00505446fc316749a850a55c
G8499 <g8499@student.jamk.fi>
2016-08-16 11:44:40 +0700

    Text Changed

--- G8499-Testaus/src/fi/jamk/iio123000/g8499/rt2/HelloTestaus.java
+++ G8499-Testaus/src/fi/jamk/iio123000/g8499/rt2/HelloTestaus.java
@@ -3,6 +3,6 @@ package fi.jamk.iio123000.g8499.rt2;
 // Testaus RT2 Mikko Siloaho G8499
 public class HelloTestaus {
 	public static void main(String[] args) {
-		System.out.println("Testaus on kivaa! G8499");
+		System.out.println("Testaus on kivaa! G8499 Eka muutos");
 	}
 }
